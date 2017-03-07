dfb2fd4bc5f890160d5c4fc046541a7bff8cd72f
EC2 Default User <ec2-user@ip-172-31-1-167.us-west-1.compute.internal>
2017-03-07 19:11:12 +0000

    test5

---
+++ TST.java
@@ -0,0 +1,18 @@
+public class TST {
+
+public TST () {
+
+}
+
+public void printTST () {
+   System.out.println("TST");
+}
+
+public static void main (String [] args) {
+
+   System.out.println("testing ... ");
+   TST tst = new TST();
+   tst.printTST();
+}
+
+}
